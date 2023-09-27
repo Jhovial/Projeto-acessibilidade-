44d62ddc373dcc5bbb9cb906efd06d327907e559
Jhovial <giovannivaailton@gmail.com>
2023-09-27 16:15:05 -0300

    aaaaa
    aaaaaaaaaaa

---
+++ forum.js
@@ -0,0 +1,26 @@
+function addComment() {
+    const commentText = document.getElementById('comment-input').value;
+
+    if (commentText.trim() !== '') {
+        const commentDiv = document.createElement('div');
+        commentDiv.className = 'comment';
+        commentDiv.innerHTML = `<p>${commentText}</p>`;
+
+        const commentsContainer = document.querySelector('.comments');
+        commentsContainer.appendChild(commentDiv);
+
+       
+        document.getElementById('comment-input').value = '';
+    }
+}
+
+
+document.getElementById('add-comment').addEventListener('click', addComment);
+
+
+document.getElementById('comment-input').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault(); 
+        addComment();
+    }
+});
