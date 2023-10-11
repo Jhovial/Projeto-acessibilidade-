e696fad62d69690328f76707ebdf19ac90342b64
Jhovial <giovannivaailton@gmail.com>
2023-10-11 18:11:34 -0300

    AAAAAAAAAAAA

--- comentarios.js
+++ comentarios.js
@@ -1,26 +1,51 @@
 function addComment() {
+
     const commentText = document.getElementById('comment-input').value;
 
+ 
+
     if (commentText.trim() !== '') {
+
         const commentDiv = document.createElement('div');
+
         commentDiv.className = 'comment';
+
         commentDiv.innerHTML = `<p>${commentText}</p>`;
 
+ 
+
         const commentsContainer = document.querySelector('.comments');
+
         commentsContainer.appendChild(commentDiv);
 
-       
+ 
+
+ 
+
         document.getElementById('comment-input').value = '';
+
     }
+
 }
 
+ 
+
+ 
 
 document.getElementById('add-comment').addEventListener('click', addComment);
 
+ 
+
+ 
 
 document.getElementById('comment-input').addEventListener('keydown', function (event) {
+
     if (event.key === 'Enter' && !event.shiftKey) {
-        event.preventDefault(); 
+
+        event.preventDefault();
+
         addComment();
+
     }
+
 });
